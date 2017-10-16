3b03d7877547194685ac5c251c71a63ae1f2378e
Luis Ruiz <luis20164@hotmail.es>
2017-10-15 23:18:19 -0500

    angular components and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'pm-root',
-  template: `<div> <h1> {{pageTitle}} </h1>
-              <div> My first component </div>
+  template: `<div > <h1> {{pageTitle}} </h1>
+              <pm-products></pm-products>
             </div>`
 })
 export class AppComponent {
